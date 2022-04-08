d8bf18f2770cac411859af0d896d03cc1404ea7b
Sergey Yanenko <syanenko65@gmail.com>
2022-04-08 07:02:33 +0300

    Cosmetics

--- Contents.m
+++ Contents.m
@@ -1,9 +1,11 @@
 % POV-Lab Toolbox
 % Version 1.00 (R2021b) 08-Apr-2022
 %
-% Documentation is <a href="matlab:doc povlab">here</a>
+% DOCUMENTATION
+% 
+% Provided as 'povlab' class <a href="matlab:doc povlab">reference</a>
 %
-% Examples
+% EXAMPLES
 %
 % <a href="matlab:if exist('./examples', 'dir') cd 'examples'; end;edit ex_surface">Surface</a> - 3D surface based on data returned by Matlab's 'surf' function
 % <a href="matlab:if exist('./examples', 'dir') cd 'examples'; end;edit ex_csg">CSG</a> - Constructive solid geometry primitives and methods
