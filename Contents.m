81619900c46c3205362bf3197779fade549dca4b
Sergey Yanenko <syanenko65@gmail.com>
2022-04-08 06:36:29 +0300

    Link to class reference

--- Contents.m
+++ Contents.m
@@ -1,20 +1,6 @@
 % POV-Lab Toolbox
-% Version 0.01 (R2021b) 09-Jan-2022
+% Version 1.00 (R2021b) 08-Apr-2022
 %
-% TODO: Complete help
-%
-% Scene initialization
-%   scene_begin -
-%   scene_end -
-%
-% Preprocessing directives
-%   include - 
-%   defile - 
-%
-% Constructive Solid Gwometry (CSG) operations
-%   union -
-%   difference -
-%   intersection -
-%   merge -
+% Documentation is <a href="matlab:doc povlab">here</a>
 %
 % Copyright 2022 Sergey Yanenko
