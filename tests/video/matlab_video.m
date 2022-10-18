514e0fd9133df8a9e8d4f00667b1e564c50227ce
Sergey Yanenko <syanenko65@gmail.com>
2022-10-19 00:24:21 +0300

    Tests removed

--- tests/video/matlab_video.m
+++
@@ -1,8 +0,0 @@
-% videoPlayer = vision.VideoPlayer;
-videoFReader = vision.VideoFileReader('Filename', 'mri.mp4',...
-                                      'PlayCount', 8);
-while ~isDone(videoFReader)
-  videoFrame = videoFReader();
-  videoPlayer(videoFrame);
-  pause(0.3)
-end
