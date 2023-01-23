65be4dbc5c3c7c5c0ac3fbd748d408cfc54dd770
Sergey Yanenko <syanenko65@gmail.com>
2023-01-23 07:01:27 +0300

    Lights example enhanced

--- examples/ex_lights_utf8.m
+++ examples/ex_lights_utf8.m
@@ -16,61 +16,60 @@ pl.scene_begin('scene_file', 'lights.pov', 'image_file', 'lights.png');
     pl.include("shapes");
     pl.global_settings("assumed_gamma 1");
     
-    pl.camera('angle', 25, 'location', [20 35 15], 'look_at', [0 10 4.5], 'type', 'perspective');
+    % Camera
+    pl.camera('angle', 30, 'location', [10 35 6], 'look_at', [0 13 5], 'type', 'perspective');
     
     % Wall
-    tex_mirror = pl.declare("tex_plane", pl.texture('pigment_odd', [0.3 0.3 0.3], 'pigment_even', [0.3 0.3 0.3], 'finish', "phong 1 reflection {0.80 metallic 0.4}"));
-    pl.plane('normal', [1,0,0], 'distance', 0, 'texture', tex_mirror, 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-
-    % External light
-    pl.light('location',  [20 35 15], 'color', [2 2 2]);
-    
+    tex_wall = pl.declare("tex_wall", pl.texture('pigment', [0.3 0.3 0.3], 'finish', "phong 1 reflection {0.05 metallic 0.2}"));
+    pl.plane('normal', [1,0,0], 'distance', 0, 'texture', tex_wall, 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
+   
     % Lights
+    rest_comp = 0.42;
     s = 4;
-    e = 14;
+    e = 10;
     n = e - s;
     for y = s:e
         r =  ((y - s) / n);
-        pl.sphere('position', [2 y 2], 'radius', 0.4, 'texture', 'Yellow_Glass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 2], 'color', [r 0.4 0.4]);
+        pl.sphere('position', [2 y * 1.5 2], 'radius', 0.4, 'texture', 'Yellow_Glass');
+        pl.light('location',  [2 y * 1.5 2], 'color', [r rest_comp rest_comp]);
     end
 
     for y = s:e
         g =  ((y - s) / e);
-        pl.sphere('position', [2 y 3], 'radius', 0.4, 'texture', 'Dark_Green_Glass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 3], 'color', [0.4 g 0.4]);
+        pl.sphere('position', [2 y * 1.5 3], 'radius', 0.4, 'texture', 'Dark_Green_Glass');
+        pl.light('location',  [2 y * 1.5 3], 'color', [rest_comp g rest_comp]);
+    end
+
+    for y = s:e
+        g =  ((y - s) / e) * 0.5;
+        pl.sphere('position', [2 y * 1.5 4], 'radius', 0.4, 'texture', 'NBwinebottle');
+        pl.light('location',  [2 y * 1.5 4], 'color', [rest_comp g rest_comp]);
     end
 
     for y = s:e
         g =  ((y - s) / e);
-        pl.sphere('position', [2 y 4], 'radius', 0.4, 'texture', 'NBwinebottle', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 4], 'color', [0.4 g 0.4]);
-    end
-
-    for y = s:e
-        g =  ((y - s) / e);
-        pl.sphere('position', [2 y 5], 'radius', 0.4, 'texture', 'NBoldglass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 5], 'color', [0.4 g 0.4]);
+        pl.sphere('position', [2 y * 1.5 5], 'radius', 0.4, 'texture', 'NBoldglass');
+        pl.light('location',  [2 y * 1.5 5], 'color', [rest_comp g rest_comp]);
     end
 
     for y = s:e
         r =  ((y - s) / e);
-        pl.sphere('position', [2 y 6], 'radius', 0.4, 'texture', 'NBglass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 6], 'color', [r 0.4 0.4]);
+        pl.sphere('position', [2 y * 1.5 6], 'radius', 0.4, 'texture', 'NBglass');
+        pl.light('location',  [2 y * 1.5 6], 'color', [r rest_comp rest_comp]);
     end
 
     for y = s:e
-        r =  ((y - s) / e);
-        pl.sphere('position', [2 y 7], 'radius', 0.4, 'texture', 'Orange_Glass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 7], 'color', [r 0.4 0.4]);
+        r =  ((y - s) / e) * 0.6;
+        pl.sphere('position', [2 y * 1.5 7], 'radius', 0.4, 'texture', 'Orange_Glass');
+        pl.light('location',  [2 y * 1.5 7], 'color', [r rest_comp rest_comp]);
     end
 
     for y = s:e
-        r =  ((y - s) / e);
-        pl.sphere('position', [2 y 8], 'radius', 0.4, 'texture', 'Vicks_Bottle_Glass', 'scale', [1 1 1], 'rotate', [0 0 0], 'translate', [0 0 0]);
-        pl.light('location',  [2 y 8], 'color', [r 0.4 0.4]);
+        g = ((y - s) / e) * 2.3;
+        pl.sphere('position', [2 y * 1.5 8], 'radius', 0.4, 'texture', 'Vicks_Bottle_Glass');
+        pl.light('location',  [2 y * 1.5 8], 'color', [ rest_comp g rest_comp]);
     end
-    
+
 pl.scene_end();
 % Render and display
 
