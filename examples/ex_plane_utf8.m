4d83b8d20e990cd4815454434fda728c2b0a0e9e
Sergey Yanenko <syanenko65@gmail.com>
2023-02-05 18:13:48 +0300

    Lightning samples

--- examples/ex_plane_utf8.m
+++ examples/ex_plane_utf8.m
@@ -13,14 +13,22 @@ pl = povlab( povray_version,...
 
 pl.scene_begin('scene_file', 'plane.pov', 'image_file', 'plane.png');
     % Camera
-    pl.camera('angle', 50, 'location', [9 9 8], 'look_at', [0 0 1], 'type', 'perspective');
+    pl.camera('angle', 50, 'location', [6 9 7] * 1.2, 'look_at', [0 0 1], 'type', 'perspective');
     
     % Lights
-    pl.light('location', [9 9 8], 'color', [3 3 3], 'shadowless', true);
-    pl.light('location', [0 0 5.5], 'color', [7 7 7], 'area_light', true, 'axis1', [10 0 0], 'axis2', [0 0 10], 'size1', 2, 'size2', 2, 'shadowless', false, 'visible', true, 'shape_color', [0.3 0.3 0.3 0.5], ...
+    % Point
+    pl.light('location', [9 9 8], 'color', [1 1 1], 'shadowless', false);
+    
+    % Point with area
+    pl.light('location', [2 1 6], 'color', [8 8 8], 'area_light', true, 'axis1', [10 0 0], 'axis2', [0 0 10], 'size1', 2, 'size2', 2, 'shadowless', false, 'visible', true, 'shape_color', [0.1 0.1 0.1 0.8], ...
              'adaptive', 4, 'area_illumination', true, 'jitter', false, 'circular', false, 'orient', false);
+
+    % Spotlight with area
+    pl.light('type', 'spotlight', 'location', [0 4 6], 'point_at', [1 3 4.5], 'color', [9 9 9], 'area_light', true, 'axis1', [10 0 0], 'axis2', [0 0 10], 'size1', 2, 'size2', 2, 'shadowless', false, 'visible', true, 'shape_color', [0.1 0.1 0.1 0.8], ...
+             'adaptive', 4, 'area_illumination', true, 'jitter', false, 'circular', false, 'orient', false);
+    
     % Axis
-    % pl.axis('length', [6 6 6]);
+    pl.axis('length', [6 6 5]);
    
     % Textures
     finish = "phong 1 reflection {0.02 metallic 0.8}";
