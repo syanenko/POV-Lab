a5bb4d6178350e93d312d8a5f2352a0e4d7cd29a
Sergey Yanenko <syanenko65@gmail.com>
2023-02-06 01:03:57 +0300

    Colored

--- examples/ex_axis_utf8.m
+++ examples/ex_axis_utf8.m
@@ -22,26 +22,13 @@ pl.include_begin('lights');
     pl.light('location', [-10 10 30],   'color', [3 3 3], 'shadowless', true);
     pl.light('location', [100 200 300], 'color', [1 1 1], 'shadowless', true);
 pl.include_end();
-% Create environment objects and put them in separate include file to save rendering time in future
-
-pl.include_begin('environment');
-    % Sky
-    pl.raw(['sky_sphere{ pigment{ gradient <0,0,1> '...
-                      'color_map{ [0   color rgb<0.01,0.01, 0.01> ] '...
-                                 '[0.4 color rgb<0.1, 0.1, 0.1>   ] '...
-                                 '[0.8 color rgb<0.2, 0.2, 0.2>   ]  '...
-                                 '[1.0 color rgb<0.5,0.5,0.5>     ]} '...
-                      'scale 2  }}']);
-pl.include_end();
-
-% Standard axis
+% Default axis
 
 pl.scene_begin('scene_file', 'axis.pov', 'image_file', 'axis.png');
     pl.include("camera");
     pl.include("lights");
-    pl.include("environment");
 
-    pl.axis('length', [4 4 4]);
+    pl.axis('length', [5 5 4]);
 pl.scene_end();
 % Render and display
 
@@ -53,13 +40,14 @@ imshow(image);
 pl.scene_begin('scene_file', 'axis_colored.pov', 'image_file', 'axis_colored.png');
     pl.include("camera");
     pl.include("lights");
-    pl.include("environment");
     
     % Texture
     tex_gray   = pl.declare("tex_gray",   pl.texture('pigment', [0.2 0.2 0.2]));
     tex_yellow = pl.declare("tex_yellow", pl.texture('pigment', [0.6 0.6 0.0]));
+    tex_pink   = pl.declare("tex_pink",   pl.texture('pigment', [0.8 0.2 0.3]*1.2));
+    tex_green  = pl.declare("tex_green",  pl.texture('pigment', [0.0 0.3 0.0]));
 
-    pl.axis('length', [4 4 4], 'radius', 0.07, 'tex_common', tex_gray, 'tex_x', tex_yellow, 'tex_y', tex_yellow, 'tex_z', tex_yellow);
+    pl.axis('length', [5 5 4], 'radius', 0.07, 'tex_common', tex_gray, 'tex_x', tex_yellow, 'tex_y', tex_pink, 'tex_z', tex_green);
 pl.scene_end();
 % Render and display
 
