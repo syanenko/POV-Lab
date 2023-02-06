293a6099bc43f225e766a454ceb68dee4c2c026c
Sergey Yanenko <syanenko65@gmail.com>
2023-02-06 04:00:28 +0300

    Enhanced

--- examples/ex_wire_box_utf8.m
+++ examples/ex_wire_box_utf8.m
@@ -5,50 +5,41 @@ ex_setup
 % Start timer
 
 tic
-% Create _povlab_ object
+% Povlab object
 
 pl = povlab( povray_version,...
              povray_path, ...
              povray_out_dir);
-% Create _camera_ object and put it in separate include file to save rendering time in future
-
-pl.include_begin('camera');
-    pl.camera('angle', 35, 'location', [5 24 9], 'look_at', [0 0 -0.6], 'type', 'perspective');
-pl.include_end();
-% Create _lights_ objects and put them in separate include file to save rendering time in future
-
-pl.include_begin('lights');
-    pl.light('location', [10 10 7],     'color', [3 3 3], 'shadowless', true);
-    pl.light('location', [-10 10 30],   'color', [3 3 3], 'shadowless', true);
-    pl.light('location', [100 200 300], 'color', [1 1 1], 'shadowless', true);
-pl.include_end();
-% Create environment objects and put them in separate include file to save rendering time in future
-
-pl.include_begin('environment');
-    % Sky
-    pl.raw(['sky_sphere{ pigment{ gradient <0,0,1> '...
-                      'color_map{ [0   color rgb<0.0,0.0, 0.0> ] '...
-                                 '[0.4 color rgb<0.01, 0.01, 0.01>   ] '...
-                                 '[0.8 color rgb<0.03, 0.03, 0.03>   ]  '...
-                                 '[1.0 color rgb<0.05, 0.05, 0.05>     ]} '...
-                      'scale 1  }}']);
-pl.include_end();
-
 % Scene
 
 pl.scene_begin('scene_file', 'wire_box.pov', 'image_file', 'wire_box.png');
     pl.include("shapes");
-    pl.include("camera");
-    pl.include("lights");
-    pl.include("environment");
+    
+    % Camera
+    pl.camera('angle', 25, 'location', [4 11 4]*1.8, 'look_at', [0 0 1.5], 'type', 'perspective');
 
-    tex_pink   = pl.declare("tex_plane_pink",   pl.texture('base', 'Silver1', 'pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.8 0.2 0.3], 'finish', "phong 1 reflection {0.3 metallic 0.8}"));
-    tex_yellow = pl.declare("tex_plane_yellow", pl.texture('base', 'Silver1', 'pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.8 0.8 0],   'finish', "phong 1 reflection {0.3 metallic 0.8}"));
-    tex_green =  pl.declare("tex_green",        pl.texture('base', 'Silver1', 'pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.0 0.8 0],   'finish', "phong 1 reflection {0.3 metallic 0.8}"));
+    % Lighgs
+    pl.light('location', [10 10 7],     'color', [3 3 3], 'shadowless', true);
+    pl.light('location', [-10 10 30],   'color', [3 3 3], 'shadowless', true);
+    pl.light('location', [100 200 300], 'color', [1 1 1], 'shadowless', true);
+   
+    % Textures
+    finish = "phong 1 reflection {0.3 metallic 0.8}";
+    tex_floor  = pl.declare("tex_floor",        pl.texture('pigment_odd', [0.01 0.01 0.01], 'pigment_even', [0.02 0.02 0.02], 'finish', "phong 1 reflection {0.02 metallic 0.8}"));
+    tex_gray   = pl.declare("tex_gray",         pl.texture('pigment', [0.2 0.2 0.2]));
+    tex_pink   = pl.declare("tex_plane_pink",   pl.texture('pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.8 0.2 0.3], 'finish', finish));
+    tex_yellow = pl.declare("tex_plane_yellow", pl.texture('pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.8 0.8 0],   'finish', finish));
+    tex_green =  pl.declare("tex_green",        pl.texture('pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.0 0.8 0],   'finish', finish));
+    tex_blue  =  pl.declare("tex_blue",         pl.texture('pigment_odd', [0.1 0.1 0.1], 'pigment_even', [0.0 0.4 0.8], 'finish', finish));
 
-    pl.wire_box('llf_corner', [-5 -4 -4], 'urb_corner', [5 4 4], 'radius', 0.07, 'texture', tex_yellow);
-    pl.wire_box('llf_corner', [-3 -2 -2], 'urb_corner', [3 2 2], 'radius', 0.07, 'texture', tex_pink);
-    pl.wire_box('llf_corner', [-4 -3 -3], 'urb_corner', [4 3 3], 'radius', 0.07, 'texture', tex_green);
+    % Axis
+    pl.axis('length', [4 4 4], 'radius', 0.025, 'tex_common', tex_gray, 'tex_x', tex_pink, 'tex_y', tex_green, 'tex_z', tex_blue);
+    
+    % Floor
+    pl.plane('normal', [0,0,1], 'texture', tex_floor);
+
+    % Box
+    pl.wire_box('llf_corner', [-2.5 -2 0], 'urb_corner', [2.5 2 2.5], 'radius', 0.03, 'texture', tex_yellow);
 
 pl.scene_end();
 
